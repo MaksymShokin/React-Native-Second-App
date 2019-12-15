39018eebbf184a0e1ddfa7492dceb6037f7a0bfe
Maksym Shokin <maksym.sh@upplabs.com>
2019-12-15 18:09:34 +0200

    'keyboard-setup'

--- screenComponents/StartGameScreen.js
+++ screenComponents/StartGameScreen.js
@@ -1,31 +1,42 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard} from 'react-native';
 
 import Card from '../components/Card';
 import Colors from '../constants/colors';
-import Input from '../components/Input'
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const inputHandler = value => {
+    setEnteredValue(value.replace(/[^0-9]/g, ''))
+  };
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}> This is start game screen! </Text>
-      <Card style={styles.inputContainer}>
-        <Text> Select a number! </Text>
-        <Input
-          style={styles.input}
-          keyboardType='number-pad'
-          maxLength={2}
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title='Reset' color={Colors.secondary}/>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.screen}>
+        <Text style={styles.title}> This is start game screen! </Text>
+        <Card style={styles.inputContainer}>
+          <Text> Select a number! </Text>
+          <Input
+            blurOnSubmit
+            style={styles.input}
+            keyboardType='number-pad'
+            maxLength={2}
+            onChangeText={inputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title='Reset' color={Colors.secondary}/>
+            </View>
+            <View style={styles.button}>
+              <Button title='Select' color={Colors.primary}/>
+            </View>
           </View>
-          <View style={styles.button} >
-            <Button title='Select' color={Colors.primary}/>
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   )
 };
 
